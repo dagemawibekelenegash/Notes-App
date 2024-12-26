9ec9c417c0297a4c2c8c0b45a37e378cf10bfbeb
dagemawinegash <dagemawibekele04@gmail.com>
2024-12-26 17:21:59 +0300

    commit for notes app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Logout from "./pages/Logout";
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
