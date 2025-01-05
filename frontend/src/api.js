6109e5361ef2f6f643a42173fe7838ad764f59e6
dagemawi bekele negash <dagemawibekele11@gmail.com>
2025-01-05 22:46:58 +0300

    updated a file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -16,7 +16,6 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
-    console.log("Token:", token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
