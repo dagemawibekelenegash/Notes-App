3753c72188ab5324a9a3559a54722b6590e3e73d
dagemawi bekele negash <dagemawibekele11@gmail.com>
2025-01-05 18:50:51 +0300

    added the code for the backend deployment purposes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,5 +145,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 CORS_ALLOWS_CREDENTIALS = True
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
